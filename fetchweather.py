d4eb2248f69b647dc8af189e3b83333f761582b9
Morten Sickel <Morten@Sickel.net>
2013-09-27 21:37:47 +0200

    Frontend for fetching data for a single point, ajax

--- fetchweather.py
+++ fetchweather.py
@@ -6,6 +6,7 @@ import datetime
 import xml.dom.minidom
 import pytz
 import json
+import re
 app = Flask(__name__)
 app.debug = True
 
@@ -13,14 +14,26 @@ cache=dict()
 
 @app.route("/ajaxserver",methods=['POST', 'GET'])
 def getweather():
-  # All input musts be validated...
+#  
+#    All input musts be validated...
+#
   lat=request.args.get('lat','')
   lon=request.args.get('lon','')
+  position=request.args.get('pos','')
+  m=re.match('(.*)N (.*)E',position)
+  if m:
+      lat=m.group(1)
+      lon=m.group(2)
   # Lat and lon can be rounded to make better caching behaviour
-  moment=request.args.get('time','')
+  moment=request.args.get('timestamp','')
+  time=request.args.get('time','')
+  date=request.args.get('date','')
+  #if(moment==''):
+  moment=date+'T'+time+'Z'    
+  print moment
   # moment must allow for less strict time specification
   url="http://api.met.no/weatherapi/locationforecastlts/1.1/?lat="+lat+";lon="+lon
-  
+  print url
   # Checks if url is cached and still not has timed out
   if (url in cache) and (cache[url+"time"]>datetime.datetime.now(pytz.utc)):
       dom=cache[url]
@@ -53,5 +66,6 @@ def home():
     return render_template('home.html')
 
 
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
--- static/fetchweather.js
+++ static/fetchweather.js
@@ -1,1 +1,37 @@
-alert("Testing!")
+window.onload=pageonload;
+url='ajaxserver'
+
+function pageonload(event){
+    Event.observe($('btFetchWeather'),'click',fetchweather);
+    $('spinner').style.visibility="hidden";
+
+}
+
+function fetchweather(event){
+    $('spinner').style.visibility="visible";
+    param=$H({ // All these values are dependent on the backend server...
+ 	a: 'tempdata'
+	,pos: $('inpLocation').value
+	,time: $('inpTime').value
+	,date: $('inpDate').value
+	,sender: event.element().id
+    });
+    ajax=new Ajax.Request(url,
+			  {method:'get',
+			   parameters: param.toQueryString(),
+			   onComplete: hHR_receiveddata}
+			 );
+}
+
+
+function hHR_receiveddata(response,json){ // The response function to the ajax call
+    if(Object.inspect(json)){
+	jsondata=response.responseText.evalJSON();
+	if(jsondata.error>''){
+	    $('error').innerHTML=jsondata.error;
+	}
+    }
+    $('result').innerHTML=jsondata.temp; //+"&deg;C";
+     $('spinner').style.visibility="hidden";
+    
+}
